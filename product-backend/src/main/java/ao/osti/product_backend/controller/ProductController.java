2ac6662e1db70ab4deb22d78db5cf568631a28d2
Socrates Muquepe <socycrates@hotmail.com>
2024-11-11 01:28:33 +0100

    Novo Controller Product

--- product-backend/src/main/java/ao/osti/product_backend/controller/ProductController.java
+++ product-backend/src/main/java/ao/osti/product_backend/controller/ProductController.java
@@ -1,12 +1,55 @@
 package ao.osti.product_backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import ao.osti.product_backend.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class ProductController {
+
+   
+
     @GetMapping("product")
-    public String getProduct() {
-        return "Eu sou um producto!";
+    public Product getProduct() {
+       Product p = new Product();
+       
+       p.setId(26);
+       p.setName("Product Name");
+       p.setPrice(250.60);
+       
+       return p;
     }
+
+    @GetMapping("products")
+    public List<Product> listProduts() {
+
+        Product p = new Product();
+        p.setId(26);
+        p.setName("Product Name");
+        p.setPrice(250.60);
+        
+        Product p1 = new Product();
+        p1.setId(31);
+        p1.setName("Product Name 31");
+        p1.setPrice(74650.60);
+
+        Product p2 = new Product();
+        p2.setId(12);
+        p2.setName("Product Name 12");
+        p2.setPrice(9936.60);
+
+        ArrayList<Product> listProduct = new ArrayList<>();
+        listProduct.add(p);
+        listProduct.add(p1);
+        listProduct.add(p2);
+
+        return listProduct;
+    }
+    
 }
