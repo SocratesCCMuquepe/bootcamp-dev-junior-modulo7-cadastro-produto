6adc0ada0e987c21cd7de97cba4d20e72a113e77
Socrates Muquepe <socycrates@hotmail.com>
2024-12-10 06:42:04 +0100

    Adicionando o endpoint para salvar produtos sem comentarios

--- product-backend/src/main/java/ao/osti/product_backend/controller/ProductController.java
+++ product-backend/src/main/java/ao/osti/product_backend/controller/ProductController.java
@@ -22,17 +22,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @CrossOrigin
 public class ProductController {
-    /*
-    private List<Product> products = Arrays.asList(
-            new Product(1, "p1roduct Name 1", "Nova descrição 1", 1, false, false, 250.60),
-            new Product(2, "Product Name 2", "Nova descrição 2", 2, false, true, 74650.60),
-            new Product(3, "Product Name 3", "Nova descrição 3", 1, false, false, 9936.60),
-            new Product(4, "Product Name 4", "Nova descrição 4", 1, true, true, 600036.60),
-            new Product(5, "Product Name 5", "Nova descrição 5", 1, true, false, 890036.60));
-    */
 
     private List<Product> products = new ArrayList<>();
-
 
     @PostMapping("product")
     public ResponseEntity<Product> save(@RequestBody Product product) {
